beb6075401f45f645ccf311068ee2c25457366a3
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-11-27 00:38:41 +0200

    refactor

--- test/e2e/Button.e2e.js
+++ test/e2e/Button.e2e.js
@@ -1,10 +1,7 @@
 describe('Button', () => {
 
   it('should have button', () => {
-    browser.get('?selectedKind=Components&selectedStory=Button');
-    var EC = protractor.ExpectedConditions;
-    browser.wait(EC.visibilityOf($('#storybook-preview-iframe')), 20000);
-    browser.switchTo().frame('storybook-preview-iframe');
+    browser.get('iframe.html?selectedKind=Components&selectedStory=Button');
 
     expect($('#main-example').getText()).toBe('Click Me!');
   }, 30000);
