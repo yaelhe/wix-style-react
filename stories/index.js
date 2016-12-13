2524120e5b9ac80f7be164ffd205428191be2de1
Ronen Amiel <ronen.amiel@gmail.com>
2016-12-13 11:02:27 +0200

    Revert "Revert "Slider updated docs""
    This reverts commit 1f7f79e55e54381c113e263997593cc632861763.

--- stories/index.js
+++ stories/index.js
@@ -15,9 +15,6 @@ storiesOf('Components', module)
   .add('Modal', () => (
       <Modal />
   ))
-  .add('Slider', () => (
-      <Slider />
-  ))
   .add('GoogleAddressInput', () => (
       <GoogleAddressInput />
   ))
