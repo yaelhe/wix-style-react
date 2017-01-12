494bf6b8bc730bf0e38a2147c23da609f5d0ccb0
Nir Hartmann <nirh@wix.com>
2017-01-12 23:23:05 +0200

    fixing lint

--- src/Label/Label.js
+++ src/Label/Label.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import typography, {convertFromUxLangToCss, convertFromCssToUxLang} from '../Typography';
+import typography, {convertFromUxLangToCss} from '../Typography';
 
 export default function Label(props) {
 
