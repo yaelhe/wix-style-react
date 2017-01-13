b2e13b7efe024fc6912e9c8648cf45784cce38c6
argshook <argshook@gmail.com>
2017-01-13 15:56:30 +0200

    EndorseContentLayout: naive render components from props

--- src/EndorseContentLayout/EndorseContentLayout.js
+++ src/EndorseContentLayout/EndorseContentLayout.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 import styles from './styles.scss';
 
-const EndorseContentLayout = () =>
+const EndorseContentLayout = ({ head, content, primaryCta, secondaryCta }) =>
   <div className={styles.root}>
-    EndorseContentLayout
+    { head && <div className={styles.head}>{head}</div> }
+    { content && <div className={styles.content}>{content}</div> }
+    { primaryCta && <div className={styles.primaryCta}>{primaryCta}</div> }
+    { secondaryCta && <div className={styles.secondaryCta}>{secondaryCta}</div> }
   </div>;
 
 EndorseContentLayout.propTypes = {
-
+  head: PropTypes.node,
+  content: PropTypes.node,
+  primaryCta: PropTypes.node,
+  secondaryCta: PropTypes.node
 };
 
 export default EndorseContentLayout;
--- src/EndorseContentLayout/EndroseContentLayout.spec.js
+++ src/EndorseContentLayout/EndroseContentLayout.spec.js
@@ -11,7 +11,11 @@ class Driver {
   }
 
   get = {
-    root: () => this.component.find(`.${styles.root}`)
+    root: () => this.component.find(`.${styles.root}`),
+    head: () => this.component.find(`.${styles.head}`),
+    content: () => this.component.find(`.${styles.content}`),
+    primaryCta: () => this.component.find(`.${styles.primaryCta}`),
+    secondaryCta: () => this.component.find(`.${styles.secondaryCta}`)
   }
 }
 
@@ -21,6 +25,24 @@ describe('EndorseContentLayout', () => {
   it('should render', () => {
     driver.when.created();
     expect(driver.get.root().length).toBe(1);
+  });
+
+  const componentsToRender = ['head', 'content', 'primaryCta', 'secondaryCta'];
+
+  it('should render children components from props', () => {
+    componentsToRender
+      .forEach(c => {
+        driver.when.created({ [c]: <div>hey hope you render</div> });
+        expect(driver.get[c]().text()).toBe('hey hope you render');
+      });
+  });
+
+  it('should not render anything when prop not given', () => {
+    componentsToRender
+      .forEach(c => {
+        driver.when.created();
+        expect(driver.get[c]().length).toBe(0);
+      });
   });
 });
 
