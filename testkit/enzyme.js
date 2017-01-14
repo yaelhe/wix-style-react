e13c74b599accd754c856756e7debbf733f67045
Lior Belinsky <liorbe@wix.com>
2017-01-15 01:51:43 +0200

    added testkit template with WixComponent and tested it both on enzyme and vanilla. added only on ButtonSelection at the moment

--- testkit/enzyme.js
+++ testkit/enzyme.js
@@ -1,3 +1,8 @@
+import {enzymeTestKitFactoryCreator} from '../src/test-common';
+
+import buttonSelectionDriverFactory from '../src/ButtonSelection/ButtonSelection.driver';
+export const buttonSelectionTestkitFactory = enzymeTestKitFactoryCreator(buttonSelectionDriverFactory);
+
 export {buttonTestkitFactory} from '../src/Button/testkit/Button.enzyme';
 export {toastTestkitFactory} from '../src/Toast/testkit/Toast.enzyme';
 export {dropdownTestkitFactory} from '../src/Dropdown/testKit/Dropdown.enzyme';
