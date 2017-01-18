a34cf3d3a3e9641a5d43cecaf5c417883952e9c9
Yael Hermon <yaelhe@wix.com>
2017-01-18 15:37:09 +0200

    fix e2e

--- src/Button/Button.e2e.js
+++ src/Button/Button.e2e.js
@@ -9,7 +9,7 @@ describe('Button', () => {
     browser.get('iframe.html?selectedKind=3.%20Buttons&selectedStory=3.1%20Standard');
 
     const EC = protractor.ExpectedConditions;
-    browser.wait(EC.visibilityOf(driver.element()), 15000);
+    browser.wait(EC.visibilityOf(driver.element()), 20000);
 
     driver.click();
     expect(driver.getButtonText()).toBe('Clicked!');
