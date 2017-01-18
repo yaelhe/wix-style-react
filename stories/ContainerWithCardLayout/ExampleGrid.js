253022cd663bc4f39f1b048d6a6ff705b18a3c9e
Mariya Feldman <mariyaf@wix.com>
2017-01-18 14:12:46 +0200

    init card

--- stories/ContainerWithCardLayout/ExampleGrid.js
+++ stories/ContainerWithCardLayout/ExampleGrid.js
@@ -1,31 +1,51 @@
 import React from 'react';
 import classNames from 'classnames';
 import grid from '../../src/Grid'
+import card from '../../src/Card'
 import styles from './ExampleGrid.scss'
 
 export default () =>
-  <div className={`${grid.container} ${grid.mainContainer} ${styles.mainContent}`}>
+<div className={styles.exampleContainer}>
+  <div className={`${grid.container} ${grid.mainContainer}`}>
     <div className={grid.row}>
-      <div className={`${grid.colXs4}`}>
-        <div className={styles.columnnContent}>column content</div>
+      <div className={grid.colXs8}>
+        <div className={card.card}>
+          <div className={card.header}>Card title</div>
+          <div className={card.content}>
+            <div className={grid.row}>
+              <div className={grid.colXs6}>card grid content</div>
+              <div className={grid.colXs6}>card grid content</div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className={`${grid.colXs4}`}>
-        <div className={styles.columnnContent}>column content</div>
-      </div>
-      <div className={`${grid.colXs4}`}>
-        <div className={styles.columnnContent}>column content</div>
+      <div className={grid.colXs4}>
+        <div className={card.card}>
+          <div className={card.header}>Card title</div>
+          <div className={card.content}>Card content</div>
+        </div>
       </div>
     </div>
 
     <div className={grid.row}>
-      <div className={`${grid.colXs4}`}>
-        <div className={styles.columnnContent}>column content</div>
+      <div className={grid.colXs4}>
+        <div className={card.card}>
+          <div className={card.header}>Card title</div>
+          <div className={card.content}>Card content</div>
+        </div>
       </div>
-      <div className={`${grid.colXs4}`}>
-        <div className={styles.columnnContent}>column content</div>
+      <div className={grid.colXs4}>
+        <div className={card.card}>
+          <div className={card.header}>Card title</div>
+          <div className={card.content}>Card content</div>
+        </div>
       </div>
-      <div className={`${grid.colXs4}`}>
-        <div className={styles.columnnContent}>column content</div>
+      <div className={grid.colXs4}>
+        <div className={card.card}>
+          <div className={card.header}>Card title</div>
+          <div className={card.content}>Card content</div>
+        </div>
       </div>
     </div>
-  </div>;
+  </div>
+</div>;
