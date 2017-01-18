1fc66cd3a57c446bb4cb9970426001fd2b674ab9
Mariya Feldman <mariyaf@wix.com>
2017-01-18 10:10:48 +0200

    fix code alignment and extract items in variable

--- stories/Breadcrubms/ExampleWithChosenActiveElement.js
+++ stories/Breadcrubms/ExampleWithChosenActiveElement.js
@@ -3,11 +3,13 @@ import styles from './ExampleBreadcrumbs.scss';
 
 import Breadcrumbs from '../../src/Breadcrumbs/Breadcrumbs';
 
+const items = [{id: '1', value: 'first item'}, {id: '2', value: 'second item'}, {id: '3', value: 'third item'}];
+
 export default () =>
   <div>
     <div className={`${styles.onGrayBackground} ${styles.exampleWrapper}`}>
       <Breadcrumbs
-        items={[{id: '1', value: 'first item'}, {id: '2', value: 'second item'}, {id: '3', value: 'third item'}]}
+        items={items}
         activeId={'3'}/>
     </div>
   </div>;
